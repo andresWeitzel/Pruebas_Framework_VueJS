d87591d796c607a5dedcbab0457f7bd5da3d738c
andresWeitzel <58787260+andresWeitzel@users.noreply.github.com>
2020-06-30 22:43:06 -0300

    Add files via upload

--- Clases/js/Clases.js
+++ Clases/js/Clases.js
@@ -2,14 +2,42 @@ const app = new Vue({
     el: '#app',
 
     data: {
-        titulo: 'Metodos',
-        input: ''
+        titulo: 'Clases',
+
+        colorParaEvento: true,
+
+        maquetadoPrincipal: [
+            'bg-primary', 'p-3', 'text-white'
+        ],
+        maquetadoSecundario: [
+            'bg-info', 'p-4', 'text-white'
+        ],
+        nuevoMaquetado: [
+            'bg-danger', 'p-5', 'text-white'
+        ],
+        botones: [
+            'btn btn-danger', 'text-white'
+        ]
+
 
     },
     methods: {
-        msj() {
-            alert('Gracias!!');
-        }
+        cambiarColorTextos() {
+
+
+            this.maquetadoPrincipal = this.nuevoMaquetado;
+            this.maquetadoSecundario = this.nuevoMaquetado;
+
+        },
+
+        deshacerColorTextos() {
+            this.maquetadoPrincipal = ['bg-primary', 'p-3', 'text-white'];
+            this.maquetadoSecundario = ['bg-info', 'p-4', 'text-white'];
+
+        },
+
+
+
     }
 
 
